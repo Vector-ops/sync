7da36effe06b36be2e0030e7ec66eca57f4ccd09
Vector-ops <sumith2827@gmail.com>
2024-02-16 20:21:23 +0530

    Refactor error handling middleware

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,16 +1,10 @@
-const {CustomError} = require('../error/all-route-catch');
-
-const errorHandler = (err, req, res, next) =>{
-    if(err instanceof CustomError) {
-        return res.status(err.status).json({
-            status: err.status,
-            message: err.message
-        });
-    }
-    return res.status(500).json({
-        status:500,
-        msg:'Unable to retrieve data. Try again later'
-    });
-}
+const errorHandler = (err, req, res, next) => {
+	const message = err.message || "Something went wrong";
+	const status = err.status || 500;
+	return res.status(500).json({
+		status,
+		message,
+	});
+};
 
 module.exports = errorHandler;
