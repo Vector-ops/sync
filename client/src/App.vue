2a94b48aa6f09ef6abbc2c2af4806490f6257f9e
Vector-ops <sumith2827@gmail.com>
2024-02-22 20:59:58 +0530

    Refactor App.vue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,41 +1,42 @@
 <template>
-  <div class="container">
-    <router-view />
-    <div class="footer">
-      <Footer />
-    </div>
-  </div>
+	<div class="container">
+		<RouterView />
+		<div class="footer">
+			<Footer />
+		</div>
+	</div>
 </template>
 
 <script>
 import Footer from "./components/Footer.vue";
+import { RouterLink, RouterView } from "vue-router";
 export default {
-  name: "App",
-  components: {
-    Footer,
-  },
+	name: "App",
+	components: {
+		Footer,
+	},
 };
 </script>
 
 <style>
 * {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
 }
 
 body {
-  font-family: "Poppins", sans-serif;
-  background: lightgray;
-  min-height: 100dvh;
-  height: 100%;
+	font-family: "Poppins", sans-serif;
+	background: lightgray;
+	min-height: 100dvh;
+	height: 100%;
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 </style>
