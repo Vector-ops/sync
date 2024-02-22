2a5c50c4a6bbc4bf6272e40bc684ee3c7765d27e
Vector-ops <sumith2827@gmail.com>
2024-02-22 21:00:02 +0530

    Add login route to router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,35 @@
 import PageNotFound from "@/views/PageNotFound.vue";
 import SingleNotePage from "@/views/SingleNotePage.vue";
+import LoginPage from "@/views/auth/LoginPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home,
-  },
-  {
-    path: "/note/:noteId",
-    name: "note",
-    component: SingleNotePage,
-  },
-  {
-    path: "/:catchAll(.*)*",
-    name: "PageNotFound",
-    component: PageNotFound,
-  },
+	{
+		path: "/",
+		name: "home",
+		component: Home,
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: LoginPage,
+	},
+	{
+		path: "/note/:noteId",
+		name: "note",
+		component: SingleNotePage,
+	},
+	{
+		path: "/:catchAll(.*)*",
+		name: "PageNotFound",
+		component: PageNotFound,
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
 
 export default router;
