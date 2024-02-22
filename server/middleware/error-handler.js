410dde31f8d64d16c83c94e1886936d31d5c3302
Vector-ops <sumith2827@gmail.com>
2024-02-22 21:00:16 +0530

    Add console.log for error debugging

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,6 +1,7 @@
 const errorHandler = (err, req, res, next) => {
 	const message = err.message || "Something went wrong";
 	const status = err.status || 500;
+	console.log(err);
 	return res.status(500).json({
 		status,
 		message,
